67cce7e7e9362846aab9db0eef0c47e971768a61
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-12-04 23:11:54 -0300

    Cria funcionalidade de converter arquivos `.seq` para `.csv`

---
+++ dataprep/harvesting/seq2csv.py
@@ -0,0 +1,120 @@
+import argparse
+
+from dataprep.harvesting.utils import seq2csv
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Data collector')
+    subparsers = parser.add_subparsers(
+        title='Command', metavar='', dest='command')
+
+    # regular_parser = subparsers.add_parser(
+    #     'seq2csv',
+    #     help=("Converte `*.seq` para `*.csv`")
+    # )
+    # regular_parser.add_argument(
+    #     'seq_file_path',
+    #     help='seq_file_path'
+    # )
+    # regular_parser.add_argument(
+    #     'csv_file_path',
+    #     help='csv_file_path'
+    # )
+
+    text_and_lang_parser = subparsers.add_parser(
+        'text_and_lang',
+        help=("Converte `text_and_lang.seq` para `text_and_lang.csv`")
+    )
+    text_and_lang_parser.add_argument(
+        'seq_file_path',
+        help='seq_file_path'
+    )
+    text_and_lang_parser.add_argument(
+        'csv_file_path',
+        help='csv_file_path'
+    )
+
+    key_and_value_parser = subparsers.add_parser(
+        'key_and_value',
+        help=("Converte `key_and_value.seq` para `key_and_value.csv`")
+    )
+    key_and_value_parser.add_argument(
+        'seq_file_path',
+        help='seq_file_path'
+    )
+    key_and_value_parser.add_argument(
+        'csv_file_path',
+        help='csv_file_path'
+    )
+
+    articles_parser = subparsers.add_parser(
+        'articles',
+        help=("Converte `articles.seq` para `articles.csv`")
+    )
+    articles_parser.add_argument(
+        'seq_file_path',
+        help='seq_file_path'
+    )
+    articles_parser.add_argument(
+        'csv_file_path',
+        help='csv_file_path'
+    )
+
+    references_parser = subparsers.add_parser(
+        'references',
+        help=("Converte `references.seq` para `references.csv`")
+    )
+    references_parser.add_argument(
+        'seq_file_path',
+        help='seq_file_path'
+    )
+    references_parser.add_argument(
+        'csv_file_path',
+        help='csv_file_path'
+    )
+
+    args = parser.parse_args()
+
+    func = None
+    fieldnames = None
+    if args.command == 'text_and_lang':
+        func = seq2csv.format_text_and_lang
+        fieldnames = ['pid', 'collection', 'lang', 'text']
+    elif args.command == 'key_and_value':
+        func = seq2csv.format_key_and_value
+        fieldnames = ['key', 'collection', 'value']
+    elif args.command == 'authors':
+        func = seq2csv.format_authors
+        fieldnames = ['key', 'collection', 'surname', 'given_names', 'orcid']
+    elif args.command == 'references':
+        func = seq2csv.format_references
+        fieldnames = [
+            'pid', 'collection',
+            'pub_date', 'vol', 'num', 'suppl', 'page',
+            'surname', 'corpauth',
+            'doi', 'journal',
+            'article_title',
+            'source',
+            'issn',
+            'thesis_date', 'thesis_loc', 'thesis_country', 'thesis_degree',
+            'thesis_org',
+            'conf_date', 'conf_loc', 'conf_country', 'conf_name', 'conf_org',
+            'publisher_loc', 'publisher_country', 'publisher_name', 'edition',
+            'source_author', 'source_corpauth',
+        ]
+    elif args.command == 'articles':
+        func = seq2csv.format_articles
+        fieldnames = ['pid', 'collection', 'aop_pid', 'issn', 'pub_date',
+                      'vol', 'num', 'suppl',
+                      'fpage', 'lpage', 'page_seq', 'elocation',
+                      'doi', 'path', 'doctopic']
+    else:
+        parser.print_help()
+
+    if fieldnames and func and args.command:
+        seq2csv.seq2csv(
+            args.seq_file_path, args.csv_file_path, fieldnames, func)
+
+
+if __name__ == "__main__":
+    main()
---
+++ dataprep/harvesting/utils/seq2csv.py
@@ -0,0 +1,219 @@
+import csv
+import os
+
+from html import unescape
+
+
+SUBFIELDS = {
+    "abstracts.seq": "a",
+    "article_titles.seq": "t",
+    "keywords.seq": "k",
+    "title_mission.seq": "*",
+}
+
+
+def entity_to_char(text):
+    return unescape(text)
+
+
+def seq2csv(seq_file_path, csv_file_path, fieldnames, fixer_func):
+    basename = os.path.basename(seq_file_path)
+    fixer = fixer_func or format_text_and_lang
+    fieldnames = fieldnames or ['pid', 'collection', 'lang', 'text']
+
+    subfield = SUBFIELDS.get(basename) or "*"
+
+    write_csv(read_seq_file(seq_file_path), fieldnames,
+              csv_file_path, fixer, subfield)
+
+
+def read_seq_file(file_path, fieldnames=None):
+    try:
+        with open(file_path, "rb") as fp:
+            for row in fp.readlines():
+                yield [item for item in row.decode("utf-8").strip().split("|")]
+    except UnicodeDecodeError:
+
+        with open(file_path, "r", encoding="iso-8859-1") as fp:
+            for row in fp.readlines():
+                try:
+                    yield [item for item in row.strip().split("|")]
+                except Exception as e:
+                    print("read_seq_file")
+                    print(e)
+                    print(row)
+
+
+def write_csv(rows, fieldnames, output_file_path, fixer, subfield=None):
+    dirname = os.path.dirname(output_file_path)
+    if not os.path.isdir(dirname):
+        os.makedirs(dirname)
+    with open(output_file_path, 'w', newline='', encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            fixed = fixer(row, subfield)
+            if not fixed:
+                continue
+            if fixer is format_key_and_value:
+                for item in fixed:
+                    writer.writerow(item)
+            else:
+                writer.writerow(fixed)
+
+
+def format_lang(raw_lang):
+    chars = [
+        c.lower()
+        for c in entity_to_char(raw_lang)
+        if c.isalpha()
+    ]
+    lang = "".join(chars)[:2]
+    return "unknown" if len(lang) < 2 else lang[:2]
+
+
+def format_references(row, subfield=None):
+    try:
+        keys = [
+            'pid',
+            'pub_date', 'vol', 'num', 'page',
+            'surname', 'corpauth',
+            'doi', 'journal',
+            'article_title',
+            'source',
+            'issn',
+            'thesis_date', 'thesis_loc', 'thesis_country', 'thesis_degree',
+            'thesis_org',
+            'conf_date', 'conf_loc', 'conf_country', 'conf_name', 'conf_org',
+            'publisher_loc', 'publisher_country', 'publisher_name', 'edition',
+            'source_author', 'source_corpauth',
+        ]
+
+        d = {k: v for k, v in zip(keys, row)}
+        d['pid'], d['collection'] = d['pid'].split("^c")
+        d['num'], d['suppl'] = (d['num'] + "^s").split("^s")[:2]
+        d['article_title'], __ = (d['article_title'] + "^l").split("^l")[:2]
+
+        return d
+
+    except ValueError as e:
+        print(e)
+        print(len(keys), len(rows))
+        print(keys)
+        print(row)
+        return {}
+
+
+def format_articles(row, subfield=None):
+    # v880,'|',v881,'|',v35,'|',v65,'|',v31,'|',v32,'|',v131,v132,'|',v14,'|',v237,'|',replace(v702,'\','/'),'|',v71
+    try:
+        pid, aop_pid, issn, pub_date, vol, num, suppl, page, doi, path, doctopic = row
+    except ValueError as e:
+        print(e)
+        print(row)
+        return {}
+    else:
+        subfields = {}
+        for item in page.split("^"):
+            if item:
+                subfields[item[0]] = item[1:]
+        data = {
+            "pid": pid,
+            "aop_pid": aop_pid,
+            "issn": issn,
+            "pub_date": pub_date,
+            "vol": vol,
+            "num": num,
+            "suppl": suppl,
+            'fpage': subfields.get("f", ''), 'lpage': subfields.get("l", ''),
+            'page_seq': subfields.get("s", ''),
+            'elocation': subfields.get("e", ''),
+            "doi": doi,
+            "path": path,
+            "doctopic": doctopic,
+        }
+        data['pid'], data['collection'] = data['pid'].split("^c")
+        return data
+
+
+def fix_value(words):
+    return " ".join([w.strip() for w in words.split(" ") if w])
+
+
+def format_key_and_value(row, subfield=None):
+    try:
+        _id = row["id"]
+        _value = row["value"]
+        collection = ''
+    except (KeyError, TypeError):
+        try:
+            _id, _value = row
+            if "^c" not in _id:
+                _id += "^c"
+        except ValueError as e:
+            _value = "|".join(row[1:])
+    _id, collection = _id.split("^c")
+    if not _value:
+        return {}
+    _value = _value.replace("/", ";")
+    items = [v for v in _value.split(";")]
+    return [
+        {"key": _id, "collection": collection, "value": fix_value(item)}
+        for item in items
+    ]
+
+
+def format_text_and_lang(row, subfield):
+    try:
+        _pid = row["pid"]
+        _text = row["text"]
+    except (KeyError, TypeError):
+        try:
+            _pid, _text = row
+        except ValueError as e:
+            _pid = row[0]
+            _text = "|".join(row[1:])
+
+    try:
+        if "^c" in _pid:
+            pid, collection = _pid.split("^c")
+        else:
+            pid = _pid
+            collection = ""
+    except:
+        #print(row)
+        return {}
+    if not _text:
+        return {}
+
+    lang = ""
+    if _text and subfield == "t" and "[title language=" in _text:
+        _text = _text[_text.find("[title language="):]
+        lang = _text[_text.find("=")+1:]
+        lang = "".join([c for c in lang if c.isalpha()])
+        lang = lang[:2]
+        if "^l" not in _text:
+            _text += "^l" + lang
+        _text = _text[_text.find("]")+1:]
+
+    if not _text.startswith("^"):
+        _text = "^*" + _text
+
+    if "^l" not in _text:
+        _text += "^lunknown"
+
+    subfields = {}
+    for item in _text.split("^"):
+        if item:
+            subfields[item[0]] = item[1:]
+    try:
+        txt = (subfields.get(subfield, '').strip() or subfields.get("*", '').strip()).strip()
+        if not txt:
+            print(row)
+            print(subfields)
+            return {}
+
+        return {"pid": pid, "collection": collection, "lang": format_lang(subfields["l"]), "text": entity_to_char(txt), }
+    except KeyError:
+        return {}
+
